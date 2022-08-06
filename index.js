97bf976facfc9ed23e0474b215c4dad2049d3c71
Eren <vrsozluk@gmail.com>
2022-08-06 14:16:14 +0300

    The big part of the game has been prepared.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let playerScore = 0
+let compScore = 0
+
 
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
@@ -5,7 +8,7 @@ function getComputerChoice() {
     return choices[randomNumber];
 } 
 
-const playRound = (playerSelection, computerSelection) => {
+function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'rock') {
         return 'You Tied!, you both picked rock'
     } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
@@ -14,15 +17,49 @@ const playRound = (playerSelection, computerSelection) => {
         return 'You tied! You both picked paper!'
 
     }else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        compScore++
         return 'you lost! computer picked scissors'
+      
     }else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++
         return 'you won! computer picked paper'
+       
     }else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++
         return 'You won! computer picked scissors!'}
-     else if (playerSelection === 'rock' && computerSelection === 'paper') {
-     return 'You lost! computer picked paper!'}
+      
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        compScore++
+        return 'You lost! computer picked paper!'}
+   
      else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        compScore++  
         return 'You lost! computer picked scissors!'}
+       
         else if (playerSelection === 'paper' && computerSelection === 'rock') {
-         return 'You won! computer picked rock!'}
+            playerScore++
+            return 'You won! computer picked rock!'}
+        
     }
+
+     
+
+    const playerSelection = prompt('rock,paper, or scissors?');
+  
+
+
+    function game()  {
+        for (let i = 0; i < 5; i++) {
+            const computerSelection = getComputerChoice()
+            console.log(playRound(playerSelection, computerSelection));
+         }if (playerScore > compScore) {
+            return 'YOU WON !'
+         }else if (playerScore<compScore) {
+            return 'you got beat by computer'
+         }else {
+            return 'you tied with the computer'
+         }
+    }
+
+alert(game())
+
