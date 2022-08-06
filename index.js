59f4b28da628357cef35561d4c4d8d10a58709bd
Eren <vrsozluk@gmail.com>
2022-08-06 13:03:18 +0300

    oyun oynama dinamikleri eklendi. örnek =  if (playerSelection === 'rock' && computerSelection === 'rock') {         return 'You Tied!, you both picked rock'

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+
 function getComputerChoice() {
-    const choices = ['r', 'p', 's'];
+    const choices = ['rock', 'paper', 'scissors'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
-}
+} 
+
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+        return 'You Tied!, you both picked rock'
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return 'You tied! You both picked scissors!'
+    }else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'You tied! You both picked paper!'
+
+    }else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'you lost! computer picked scissors'
+    }else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'you won! computer picked paper'
+    }else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You won! computer picked scissors!'}
+     else if (playerSelection === 'rock' && computerSelection === 'paper') {
+     return 'You lost! computer picked paper!'}
+     else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lost! computer picked scissors!'}
+        else if (playerSelection === 'paper' && computerSelection === 'rock') {
+         return 'You won! computer picked rock!'}
+    }
